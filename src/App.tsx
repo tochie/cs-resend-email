309813c1e23539be077ba6063eb74094923652b2
Tochi Eke-Okoro <tochie@gmail.com>
2023-11-03 19:28:42 -0400

    Update App.tsx
    Updated to use useEffect for proper syncing with fresh state vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+// import fetch from "fetch";
 
+const resendSrvc =
+  "https://service-lb-ebe7c81d6c09a70d.elb.us-east-1.amazonaws.com/email/resend";
 const ResendEmailForm = () => {
   const [email, setEmail] = useState("");
   const [orderNumber, setOrderNumber] = useState("");
-  const [eventType, setEventType] = useState("");
+  const [candidateType, setCandidateType] = useState("");
+  const [cancellationType, setCancellationType] = useState("");
 
   const [submitButtonIsDisabled, setSubmitButtonIsDisabled] = useState(true);
+
+  useEffect(() => {
+    // Check if all form fields are completed with no issues
+    const isAllFieldsCompleted = !!(email && orderNumber && candidateType);
+
+    // Enable or disable the submit button based on the validation result
+    setSubmitButtonIsDisabled(!isAllFieldsCompleted);
+  }, [email, orderNumber, candidateType]);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    // Make an async call to the API endpoint or service here
-    // ...
+    try {
+      // Make an async call to the API endpoint or service here
+      const response = await fetch(resendSrvc, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email,
+          orderNumber,
+          candidateType,
+          cancellationType
+        })
+      });
 
-    // If the call is successful, reset the form
-    setEmail("");
-    setOrderNumber("");
-    setEventType("");
+      console.log("response is: ", response);
+
+      if (response.ok) {
+        // If the call is successful, reset the form
+        setEmail("");
+        setOrderNumber("");
+        setCandidateType("");
+        setCancellationType("");
+        setSubmitButtonIsDisabled(true); // Disable the submit button after successful submission
+        alert("Email resent successfully!");
+      } else {
+        alert("Failed to resend email. Please try again.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+      alert("An error occurred. Please try again.");
+    }
   };
 
   const handleInputChange = (e: any) => {
     const { name, value } = e.target;
-
+    console.log("input onChange: ", value);
     if (name === "email") {
       setEmail(value);
     } else if (name === "orderNumber") {
       setOrderNumber(value);
-    } else if (name === "eventType") {
-      setEventType(value);
+    } else if (name === "candidateType") {
+      setCandidateType(value);
+    } else if (name === "cancellationType") {
+      setCancellationType(value);
     }
-
-    // Check if all form fields are completed with no issues
-    const isAllFieldsCompleted = email && orderNumber && eventType;
-
-    // Enable or disable the submit button based on the validation result
-    setSubmitButtonIsDisabled(!isAllFieldsCompleted);
   };
 
   return (
@@ -53,7 +86,7 @@ const ResendEmailForm = () => {
             name="email"
             placeholder="Email"
             value={email}
-            onChange={handleInputChange}
+            onInput={handleInputChange}
           />
 
           <input
@@ -62,25 +95,35 @@ const ResendEmailForm = () => {
             name="orderNumber"
             placeholder="Order Number"
             value={orderNumber}
-            onChange={handleInputChange}
+            onInput={handleInputChange}
           />
 
           <select
-            name="eventType"
-            value={eventType}
+            name="candidateType"
+            value={candidateType}
             onChange={handleInputChange}
-            className="eventType"
+            className="candidateType"
+          >
+            <option value="">Select event type</option>
+            <option value="bopis_ready_for_pickup">
+              BOPIS ready for pickup
+            </option>
+          </select>
+
+          <select
+            name="cancellationType"
+            value={cancellationType}
+            onChange={handleInputChange}
+            className="candidateType"
           >
             <option value="">Select event type</option>
             <option value="orderConfirmation">Order confirmation</option>
-            <option value="shippingNotification">Shipping notification</option>
-            <option value="returnConfirmation">Return confirmation</option>
           </select>
 
           <button
             type="submit"
             disabled={submitButtonIsDisabled}
-            className="resend"
+            className={`resend-disabled-${submitButtonIsDisabled}`}
           >
             Resend
           </button>
